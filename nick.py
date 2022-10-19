7b82df2dcce3a5e394b26bae64f3106dbb13ab0d
fliskScript <100136481+fliskScript@users.noreply.github.com>
2022-10-19 19:56:53 +0300

    Update nick.py

--- nick.py
+++ nick.py
@@ -1,9 +1,2 @@
-#imports
-import random
-import string
-#variables
-a_list=['super','krasavchik','gamer','mega']
-b_list=['colider','loshara','genii','stroitel','suka','minecrafter', 'brawler']
-c_list=['2006','2007','2008','2009','2010','2012','2013','2014','2015']
-#code
-print("YOUR NICK -", random.choice(a_list),random.choice(b_list),random.choice(c_list))
+key = admin
+keyy = FFG82KA
